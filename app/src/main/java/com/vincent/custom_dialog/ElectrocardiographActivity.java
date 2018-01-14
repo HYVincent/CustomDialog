fad0ec1b1e465be9dbccfa570273d54c94e666bf
Vincent <1032006226@qq.com>
2018-01-15 07:43:42 +0800

    2018年1月15日07:43:41

--- app/src/main/java/com/vincent/custom_dialog/ElectrocardiographActivity.java
+++ app/src/main/java/com/vincent/custom_dialog/ElectrocardiographActivity.java
@@ -5,9 +5,6 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
-import com.vincent.custom_dialog.view.Electrocardiogram;
-import com.vincent.custom_dialog.view.MyElectrocardiographView;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,29 +20,12 @@ import java.util.List;
 
 public class ElectrocardiographActivity extends AppCompatActivity {
 
-    private Electrocardiogram electrocardiogram;
     private String data;
-    private MyElectrocardiographView myElectrocardiographView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_electrocardiograph);
         data = ReadAssetsUtils.readAssetsTextReturnStr(this,"StarCareData");
-        electrocardiogram = findViewById(R.id.e_electrocardiogram);
-        electrocardiogram.addData();
-        electrocardiogram.setElectrocardiogramData(getData(data));
-        electrocardiogram.startDraw();
-        myElectrocardiographView = findViewById(R.id.me_myelectrocardiographview);
-    }
-
-    private List<Float> getData(String data) {
-        List<Float> datas = new ArrayList<>();
-        String[] strarr = data.split("\n");
-        for (int i=0;i<strarr.length;i++){
-            datas.add(Float.valueOf(strarr[i]));
-        }
-        Log.d(getClass().getSimpleName(), "getData: "+datas.size());
-        return datas;
     }
 }
--- app/src/main/java/com/vincent/custom_dialog/ViewActivity.java
+++ app/src/main/java/com/vincent/custom_dialog/ViewActivity.java
@@ -1,16 +1,16 @@
 package com.vincent.custom_dialog;
 
-import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
-import android.text.TextUtils;
-import android.util.Log;
+import android.text.method.KeyListener;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.vincent.dialog.util.MyToast;
-
-import java.util.ArrayList;
 
 /**
  * @author Vincent QQ:1032006226
@@ -23,15 +23,50 @@ import java.util.ArrayList;
 
 public class ViewActivity extends AppCompatActivity {
 
+    private KeyListener keyListener;
+    private EditText editText;
+    private Button button;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view);
-        findViewById(R.id.button_electrocardiograph).setOnClickListener(new View.OnClickListener() {
+        editText = findViewById(R.id.edit_query);
+        button = findViewById(R.id.buttonPanel);
+        editTextable(editText,false);
+        button.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
             @Override
-            public void onClick(View view) {
-                startActivity(new Intent(ViewActivity.this,ElectrocardiographActivity.class));
+            public void onClick(View v) {
+                editTextable(editText,true);
+                MyToast.toastMsg(ViewActivity.this,"click");
+            }
+        });
+        findViewById(R.id.open).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditTextUtils.openKeyboard(ViewActivity.this,editText);
+            }
+        });
+        findViewById(R.id.close).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditTextUtils.closeKeyboard(ViewActivity.this);
             }
         });
     }
+
+    //设置EditText可输入和不可输入状态
+    private void editTextable(EditText editText, boolean editable) {
+        if (!editable) { // disable editing password
+            editText.setFocusable(false);
+            editText.setFocusableInTouchMode(false); // user touches widget on phone with touch screen
+            editText.setClickable(false); // user navigates with wheel and selects widget
+        } else { // enable editing of password
+            editText.setFocusable(true);
+            editText.setFocusableInTouchMode(true);
+            editText.setClickable(true);
+        }
+    }
+
 }
